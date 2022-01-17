47be6561e5cf3a110da4b259dc714c93ce814476
Codenameaidan <ajw137@pitt.edu>
2022-01-17 11:41:17 -0500

    Updated mobile layout

--- src/app/Components/navbar/navbar.component.ts
+++ src/app/Components/navbar/navbar.component.ts
@@ -9,10 +9,18 @@ import { Router } from '@angular/router';
 export class NavbarComponent implements OnInit {
 
   constructor(public router : Router ) { }
+  mobile:boolean = false;
+  menu_on:boolean = false;
 
   ngOnInit(): void {
+      //alert(window.screen.width);
+      if (window.screen.width <= 520) { // 768px portrait
+        this.mobile = true;
+      }
+  }
 
-    
+  toggle_menu(): void{
+    this.menu_on = !this.menu_on;
   }
 
 }
